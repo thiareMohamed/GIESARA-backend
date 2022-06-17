53e571cf8699f581a29e39736c80db2efb126e37
thiaremohamed.mt@gmail.com <thiaremohamed.mt@gmail.com>
2022-06-17 12:05:32 +0000

    Add Entity Town and Common

---
+++ src/main/java/sn/thiare/GIESARA/backend/model/Commune.java
@@ -0,0 +1,23 @@
+package sn.thiare.GIESARA.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Commune {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
---
+++ src/main/java/sn/thiare/GIESARA/backend/model/Town.java
@@ -0,0 +1,23 @@
+package sn.thiare.GIESARA.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Town {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Common common;
+}
