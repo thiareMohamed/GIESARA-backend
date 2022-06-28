1eda1a90168c24499c2afbe63c552bb10e42465a
thiaremohamed.mt@gmail.com <thiaremohamed.mt@gmail.com>
2022-06-28 09:57:53 +0000

    add account controller

---
+++ src/main/java/sn/thiare/GIESARA/backend/service/web/CompteController.java
@@ -0,0 +1,43 @@
+package sn.thiare.GIESARA.backend.service.web;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+import sn.thiare.GIESARA.backend.model.Role;
+import sn.thiare.GIESARA.backend.model.Utilisateur;
+import sn.thiare.GIESARA.backend.security.CompteService;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class CompteController {
+
+    private CompteService compteService;
+    public CompteController(CompteService compteService) {
+        this.compteService = compteService;
+    }
+
+    @GetMapping("/utilisateurs")
+    public List<Utilisateur> getAllUtilisateurs() {
+        return compteService.getAllUtilisateurs();
+    }
+
+    @PostMapping("/utilisateurs")
+    public Utilisateur createUtilisateur(@RequestBody Utilisateur utilisateur) {
+        return compteService.createUtilisateur(utilisateur);
+    }
+
+    @PostMapping("/roles")
+    public Role createRole(@RequestBody Role role) {
+        return compteService.createRole(role);
+    }
+    @PostMapping("addRoleToEmail")
+    public void addRoleToEmail(@RequestBody RoleEmailForm roleEmailForm) {
+        compteService.addRoleToEmail(roleEmailForm.getEmail(), roleEmailForm.getLibelle());
+    }
+}
+@Data
+class RoleEmailForm {
+    String email;
+    String libelle;
+}
